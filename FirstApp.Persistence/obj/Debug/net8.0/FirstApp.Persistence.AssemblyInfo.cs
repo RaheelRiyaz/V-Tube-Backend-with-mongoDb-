4766c3f9b1aac74f11c6fba19de72effeee34a46
RaheelRiyaz <132251877+RaheelRiyaz@users.noreply.github.com>
2024-05-17 16:57:37 +0530

    Changes

--- FirstApp.Persistence/obj/Debug/net8.0/FirstApp.Persistence.AssemblyInfo.cs
+++ FirstApp.Persistence/obj/Debug/net8.0/FirstApp.Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstApp.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d64445cbece40384a028951b730c7cef7be75668")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+584d1ec13b3b04dba21333af5ec67181aa98bd00")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstApp.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstApp.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
